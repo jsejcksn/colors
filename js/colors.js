4532d771efb4451880e008cec0eccc4400872178
Jesse Jackson <jsejcksn@users.noreply.github.com>
2015-09-16 02:55:12 -0500

    Use strict, Beautify js

--- js/colors.js
+++ js/colors.js
@@ -1,23 +1,24 @@
 (function() {
+  'use strict';
 
-var main = document.getElementById('colors');
+  var main = document.getElementById('colors');
 
-for (var a = 0; a < 20; a++) {
-  for (var b = 1; b < 8; b++) {
-    main.innerHTML += '<section class="c' + b + '"></section>';
+  for (var a = 0; a < 20; a++) {
+    for (var b = 1; b < 8; b++) {
+      main.innerHTML += '<section class="c' + b + '"></section>';
+    }
   }
-}
 
-function scrollDown(i) {
-  setTimeout(function() {
-    window.scrollTo(0, i);
-    console.log(i);
-  }, i/2);
-}
+  function scrollDown(i) {
+    setTimeout(function() {
+      window.scrollTo(0, i);
+      console.log(i);
+    }, i / 2);
+  }
 
 
-// for (var i = 1; i < 34001; i++) { // Autoscroll
-//   scrollDown(i);
-// }
+  // for (var i = 1; i < 34001; i++) { // Autoscroll
+  //   scrollDown(i);
+  // }
 
 }());
